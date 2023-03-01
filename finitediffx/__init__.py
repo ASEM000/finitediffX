d8b3a63afb11ee1443a1838377d40eaa1d136a47
ASEM000 <mahmoudasem00@gmail.com>
2023-03-01 14:35:01 +0900

    initial

---
+++ finitediffx/__init__.py
@@ -0,0 +1,23 @@
+from ._src.fgrad import fgrad
+from ._src.finite_diff import (
+    curl,
+    difference,
+    divergence,
+    gradient,
+    hessian,
+    jacobian,
+    laplacian,
+)
+from ._src.utils import generate_finitediff_coeffs
+
+__all__ = (
+    "curl",
+    "divergence",
+    "difference",
+    "gradient",
+    "jacobian",
+    "laplacian",
+    "hessian",
+    "fgrad",
+    "generate_finitediff_coeffs",
+)
